c3380e42d0254a90a932931ce01ae4231dedad14
DEV <dev@info.local>
2020-12-10 16:05:22 -0300

    finalizado

---
+++ classes/MegaMen.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace POO_Polimorfismo.classes
+{
+    public class MegaMen : Player
+    {
+        public override void Correr(){
+            Console.WriteLine("corrnod em 100X");
+        }
+    }
+}
